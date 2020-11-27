e8e781096f9cfc249b5e7cb3d513a907780b16eb
Jeon Hyejin <gen_nie@naver.com>
2020-11-27 21:39:55 +0900

    rotate camera

---
+++ 2PM/Assets/CameraMoveController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMoveController : MonoBehaviour, GameInputAction.IPlayerCameraActions
+{
+    private GameInputAction _inputAction;
+
+    private Vector2 _mouseDeltaVector;
+
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float mouseSensitivity = 100f;
+    
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        if(_inputAction == null) _inputAction = new GameInputAction();
+        
+        _inputAction.PlayerCamera.SetCallbacks(this);
+        _inputAction.PlayerCamera.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputAction.Disable();
+    }
+    
+    void Update()
+    {
+        var horizontalDirection = _mouseDeltaVector.x * Time.deltaTime * mouseSensitivity;
+        transform.Rotate(0, horizontalDirection, 0);
+    }
+    
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        _mouseDeltaVector = context.ReadValue<Vector2>();
+    }
+}
---
+++ 2PM/Assets/PlayerMoveController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoveController : MonoBehaviour, GameInputAction.IPlayerActions
+{
+    private CharacterController _characterController;
+    private GameInputAction _inputAction;
+    private Vector2 _moveActionValue;
+    
+    [SerializeField] private float characterMoveSpeed = 10f;
+    
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    
+    void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        if (_inputAction == null)
+            _inputAction = new GameInputAction();
+        
+        _inputAction.Player.SetCallbacks(this);
+        _inputAction.Player.Enable();
+    }
+
+    void Update()
+    {
+        var verticalVector = transform.forward * (_moveActionValue.y * Time.deltaTime * characterMoveSpeed);
+        var horizontalVector = transform.right * (_moveActionValue.x * Time.deltaTime * characterMoveSpeed);
+        _characterController.Move(verticalVector + horizontalVector);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveActionValue = context.ReadValue<Vector2>();
+    }
+    
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
