0591142b047fa68976cddfcf8f83e185fa326fa7
Jeon Hyejin <gen_nie@naver.com>
2020-12-02 07:20:50 +0900

    player aim & game manager

---
+++ 2PM/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonBehaviour<GameManager>
+{
+    public GameState state;
+
+    private void Start()
+    {
+        EventManager.On("game_started", OnGameStart);
+    }
+
+    private void OnGameStart(object obj)
+    {
+        state = GameState.Playing;
+    }
+}
