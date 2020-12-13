6e0cc28a88dca801c689e00676e342128cd9b52f
uhbbang3 <ansdnjswjd33@gmail.com>
2020-12-13 17:34:51 +0900

    피자 발사 구현

---
+++ 2PM/Assets/Scripts/Pizza/FirePizza.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePizza : MonoBehaviour
+{
+    public GameObject pizza;
+    public Transform firePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+    
+    void Fire()
+    {
+        Instantiate(pizza, firePos.position, firePos.rotation);
+    }
+}
