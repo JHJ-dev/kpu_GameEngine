fe9c27d0e481a401f212ec97edc9e73f23bb9990
uhbbang3 <ansdnjswjd33@gmail.com>
2020-12-14 15:09:19 +0900

    피자 날리기 구현 완료

---
+++ 2PM/Assets/Scripts/Pizza/FirePizza.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePizza : MonoBehaviour
+{
+    public GameObject pizza;
+    public Transform firePos;
+
+    public Transform dir;
+    Vector3 _dir;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Instantiate(pizza, firePos.position, dir.rotation);
+
+        _dir = new Vector3(dir.position.x - firePos.position.x, dir.position.y - firePos.position.y, dir.position.z - firePos.position.z).normalized;
+        
+        pizza.transform.forward += _dir;
+    }
+}
---
+++ 2PM/Assets/Scripts/Pizza/PizzaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaController : MonoBehaviour
+{
+    public float speed = 100.0f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Building")
+        {
+            Destroy(gameObject);
+        }
+
+        Destroy(gameObject, 3.0f);
+    }
+}
