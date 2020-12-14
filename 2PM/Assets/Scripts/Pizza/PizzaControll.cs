e1083fb4e549000c7b4f18587ff7c4d15ab0f029
uhbbang3 <ansdnjswjd33@gmail.com>
2020-12-15 08:39:35 +0900

    점수 변경 완료

--- 2PM/Assets/Scripts/Pizza/PizzaControll.cs
+++ 2PM/Assets/Scripts/Pizza/PizzaControll.cs
@@ -6,13 +6,22 @@ public class PizzaControll : MonoBehaviour
 {
     public float speed = 100.0f;
 
+    public int score;
+    public PrintScore pScore;
+
     void Start()
     {
         GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+
+        pScore = GameObject.Find("Score").GetComponent<PrintScore>();
+        
+        score = 0;
     }
 
     void Update()
     {
+        
+        Destroy(gameObject, 3.0f);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -21,7 +30,11 @@ public class PizzaControll : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
-        Destroy(gameObject, 3.0f);
+        if (collision.collider.tag == "DeliveryCube")
+        {
+            score = 100;
+            pScore.score += score;
+            Destroy(gameObject);
+        }
     }
 }
---
+++ 2PM/Assets/Scripts/UI/PrintScore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintScore : MonoBehaviour
+{
+    private Text _text;
+    public int score;
+    //PizzaControll pizza;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+        score = 0;
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance.state == GameState.Initialized)
+        {
+            _text.text = "";
+        }
+
+        if (GameManager.Instance.state == GameState.Playing)
+        {
+            _text.text = $"점수:{score}";
+        }
+    }
+}
